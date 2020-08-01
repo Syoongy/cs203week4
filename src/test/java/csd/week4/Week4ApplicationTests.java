9fdb458941ef019986ae11aac4030c8cac7d4646
Don Ta <donta@smu.edu.sg>
2020-08-01 14:23:19 +0800

    "Clean up code"

--- src/test/java/csd/week4/Week4ApplicationTests.java
+++ src/test/java/csd/week4/Week4ApplicationTests.java
@@ -1,10 +1,10 @@
-package csd.week3;
+package csd.week4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Week4ApplicationTests {
 
 	@Test
 	void contextLoads() {
